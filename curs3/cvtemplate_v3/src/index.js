38e132d71da33a138d455ab0784da6160d4fb056
Profesor C1 <admin@PROF-C1.local>
2025-03-13 17:04:43 +0200

    curs3

--- curs3/cvtemplate_v3/src/index.js
+++ curs3/cvtemplate_v3/src/index.js
@@ -1,16 +1,18 @@
 // Import our custom CSS
-import './assets/scss/styles.scss'
+import './scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
 // Import https://github.com/VincentGarreau/particles.js
 import particles from 'particles.js'
-import './assets/js/particles.js'
+import './js/particles.js'
 
 
 // Import https://github.com/mattboldt/typed.js/
 // import Typed from 'typed.js';
 import '../node_modules/typed.js/dist/typed.umd.js'
 
-import './assets/js/main.js';
+import './js/main.js';
+
+import "./js/skill.js";
